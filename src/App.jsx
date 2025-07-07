6d9f16d18cdbeaa0c19dc22083436bb8edf2f473
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 23:28:04 +0100

    updated small

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import { Suspense, lazy, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './components/theme-provider';
+
+// Main pages
+const Home = lazy(() => import('./pages/Home'));
+
 function App() {  
   return (
     <div>Hello world</div>
