10b7d0f70b6a25b7cc7bc5c124caa7efaf6c2916
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 16:19:49 +0100

    update app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <h1>Vite + React</h1>
-    </>
+    <div className='bg-red-700 text-7xl font-bold'>
+      Hello World
+    </div>
   )
 }
 
