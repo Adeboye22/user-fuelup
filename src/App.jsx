2f28b197b43e3c16e1ec6c6509ec8cd83348f724
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 20:52:50 +0100

    deleted tailwindcss animate

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import { Suspense, lazy, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+import React from 'react'
 
-import ScrollToTop from './Components/ScrollToTop';
-import { ThemeProvider } from './components/theme-provider';
-
-// Main pages
-const Home = lazy(() => import('./pages/Home'));
-
-function App() {  
-
+const App = () => {
   return (
-      <Router>
-        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-          <Toaster position="top-right" />
-          <Routes>
-            {/* Main routes with MainLayout - wrap with PublicRoute */}
-            <Route index element={<Home />} />
-          </Routes>
-          <ScrollToTop />
-        </ThemeProvider>
-      </Router>
-  );
+    <div>
+      App
+    </div>
+  )
 }
 
-export default App;
+export default App
