f01ca35e4b49f238a5b18a0e7110b9bbbb96f411
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 17:11:55 +0100

    made a change

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='bg-red-700 text-7xl font-bold'>
-      Hello World
+      Hello World and heres anothere change
     </div>
   )
 }
