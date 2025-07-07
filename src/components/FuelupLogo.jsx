a1f940ffadd3ccc108f29b96afcad60597326a2a
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 19:43:24 +0100

    added new files to the component folder

---
+++ src/components/FuelupLogo.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FuelupLogo = () => {
+  return (
+    <>
+    {/* <div className="flex items-center gap-2">
+      <div className="flex items-center justify-center w-12 h-12 rounded-full bg-emerald-500 text-white font-bold text-lg">
+        FU
+      </div>
+      <div className="text-2xl font-semibold text-gray-100">FuelUp</div>
+    </div> */}
+
+    <img src="/icons/Logo.png" className='h-14' alt="fuel logo" />
+    </>
+  )
+}
+
+export default FuelupLogo
