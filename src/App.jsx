da5d1b16ca5884734740e17826c33761ae3eafaf
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-07 23:27:07 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-import { Suspense, lazy, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ThemeProvider } from './components/theme-provider';
-
-// Main pages
-const Home = lazy(() => import('./pages/Home'));
-
 function App() {  
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div>Hello world</div>
-    </ThemeProvider>
+    <div>Hello world</div>
   );
 }
 
