267411e105403e8068d02e9340cd9e07d62c05ea
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-08 00:19:46 +0100

    fght

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
-
-import { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "./components/theme-provider";
-
-const MainLayout = lazy(() => import("./layouts/MainLayout"));
-const Home = lazy(() => import("./pages/Home"));
-
-function App() {
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './components/theme-provider';
+import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
+function App() {  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
-        <Suspense fallback={<div style={{ textAlign: "center" }}>Loading…</div>}>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route index element={<Home />} />
-            </Route>
-          </Routes>
-        </Suspense>
+        <Routes>
+          <Route index element={<Home />} />
+        </Routes>
       </Router>
     </ThemeProvider>
   );
