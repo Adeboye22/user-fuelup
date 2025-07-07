f05ece471a9706010ebe50a9110d11a547f4c435
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-08 00:05:55 +0100

    upaded app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
+import { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './components/theme-provider';
 import Home from './pages/Home';
+import PublicRoute from './routes/PublicRoute';
+import MainLayout from './layouts/MainLayout';
 function App() {  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
         <Routes>
-          <Route index element={<Home />} />
+          {/* Main routes with MainLayout - wrap with PublicRoute */}
+          <Route element={
+            <Suspense>
+              <PublicRoute>
+                <MainLayout />
+              </PublicRoute>
+            </Suspense>
+          }>
+            <Route index element={<Home />} />
+          </Route>
         </Routes>
       </Router>
     </ThemeProvider>
