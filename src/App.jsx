e7621ac646211fbe9f5c36f1a713ed20ec0aee5d
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-08 01:00:13 +0100

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from './components/theme-provider';
 import Home from './pages/Home';
+import MainLayout from './layouts/MainLayout';
 function App() {  
   return (
     <ThemeProvider defaultTheme="dark" storageKey="fuelup-ui-theme">
       <Router>
         <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<Home />} />
+          {/* Public Routes with MainLayout */}
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+
         </Routes>
       </Router>
     </ThemeProvider>
