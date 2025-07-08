faf4e33cefeacbfe4363be0b35134ebeac91a97a
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-08 13:37:15 +0100

    fixed the hero home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import Hero from '@/components/Hero';
+import { Truck } from 'lucide-react';
 import React, { useEffect } from 'react';
+import Marquee from 'react-fast-marquee';
 import { FaChevronDown } from 'react-icons/fa';
 
 const Home = () => {
@@ -7,7 +9,7 @@ const Home = () => {
     // Scroll to top on page load
     window.scrollTo(0, 0);
   }, []);
-  
+
   return (
     <div className="text-gray-100 font-sans">
       {/* Hero Section with fullscreen height */}
@@ -33,6 +35,27 @@ const Home = () => {
           </div>
         </div>
       </section>
+
+      {/* Announcement Bar */}
+      <div className="bg-yellow-600/80 shadow-lg">
+        <div className="container mx-auto py-3">
+          <Marquee
+            speed={40}
+            pauseOnHover={true}
+            gradient={false}
+            className="text-white"
+          >
+            <p className="text-lg font-medium mx-4">You don't have to compromise your comfort just to get fuel. You order from the nearest filling station, we deliver!</p>
+            <span className="text-2xl mx-4 inline-block scale-x-[-1] text-emerald-600 rounded-full bg-emerald-800/10 p-4">
+              <Truck />
+            </span>
+            <p className="text-lg font-medium mx-4">Petrol (PMS) at N00.00, Diesel (AGO) at N00.00, Kerosene (DPK) at N00.00.</p>
+            <span className="text-2xl mx-4 inline-block scale-x-[-1] text-emerald-600 rounded-full bg-emerald-800/10 p-4">
+              <Truck />
+            </span>
+          </Marquee>
+        </div>
+      </div>
     </div>
   );
 };
