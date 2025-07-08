3136d51c57f9dba082840b35b90a9cdac3f9fd49
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-08 13:17:22 +0100

    fixed dashboard page

---
+++ src/pages/dashboard/DashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardHome = () => {
+  return (
+    <div className='text-7xl font-bold bg-red-700'>
+      dashboardHome
+    </div>
+  )
+}
+
+export default DashboardHome
