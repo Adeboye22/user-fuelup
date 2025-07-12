cea3379cb090f5c71d52c8cd7159ac9bdb89a1b4
Onyenokwe Great Chukwuka <chukwukaonyenokwe@gmail.com>
2025-07-12 18:50:25 +0100

    implemented sitemap to the project

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,29 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import generateSitemap from "vite-plugin-sitemap"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    generateSitemap({
+      hostname: 'https://www.fuelupng.com/',
+      robots: [
+        {
+          userAgent: '*',
+          allow: '/',
+        }
+      ],
+      dynamicRoutes: [
+        '/',
+        '/signin',
+        '/signup', 
+        '/forgot-password'
+      ]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
